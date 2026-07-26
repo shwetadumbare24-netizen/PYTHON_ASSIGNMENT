09c82ee501ee925ebb883d73a180cab8f8ecc915
shwetadumbare24-netizen <shwetadumbare24@gmail.com>
2026-07-26 14:35:09 +0530

    first commit

---
+++ string_practice.py
@@ -0,0 +1,8 @@
+student_name = "sonu"
+stud_surname = 'dumbare'
+stud_info = """wsedrftgyhujkqwsedrftgyhujiazsxdcfvgbhnjmk"""
+stud_address = '''wsedrftgyhujkqwsedrftgyhujiazsxdcfvgbhnjmk'''
+print(student_name)
+print(stud_surname)
+print(stud_info)
+print(stud_address)
