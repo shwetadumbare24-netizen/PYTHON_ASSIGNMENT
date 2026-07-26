56f9e03b8356841e7da1380e09ed8d19f76dcbb3
shwetadumbare24-netizen <shwetadumbare24@gmail.com>
2026-07-26 22:49:39 +0530

    second commit

--- string_practice.py
+++
@@ -1,8 +0,0 @@
-student_name = "sonu"
-stud_surname = 'dumbare'
-stud_info = """wsedrftgyhujkqwsedrftgyhujiazsxdcfvgbhnjmk"""
-stud_address = '''wsedrftgyhujkqwsedrftgyhujiazsxdcfvgbhnjmk'''
-print(student_name)
-print(stud_surname)
-print(stud_info)
-print(stud_address)
